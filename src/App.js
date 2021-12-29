68332010f80c0bbc25a0ee0efc3dd4aa11349e6c
Jake Zakrzewski <j.zakrzewsky@gmail.com>
2021-12-29 14:39:25 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
-import React, { useState, useRef, useEffect } from "react";
-import TodoList from "./TodoList";
+import React, { useState, useRef, useEffect } from 'react' 
 import { v4 as uuidv4 } from 'uuid';
+import TodoList from './TodoList'
 
-const LOCAL_STORAGE_KEY = 'todosApp.todos'
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef();
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
 
-  useEffect(() => {
+  useEffect (() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(storedTodos)
   }, [])
+
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
-  function handleAddTodo (e) {
-    const name = todoNameRef.current.value;
-    if (name === '') return
-    console.log(name)
-    setTodos(prevTodos => {
-      return [prevTodos, {id: uuidv4(), name: name, complete: false}]
-    })
-    todoNameRef.current.value = null;
+  function toggleTodo (id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
   }
 
-    return (
-      <>
-        <TodoList todos={todos} />
-        <input ref={todoNameRef} type="text" />
-        <button onClick={handleAddTodo}>Add Todo</button>
-        <button>Clear Completed Todos</button>
-        <div>0 left to do</div>
-      </>
-    );
+  function addTodo (e) {
+    // e.preventDefault;
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function clearCompletedTodos () {
+    const newTodos = todos.filter(todo => !todo.complete)
+    console.log(newTodos)
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={addTodo}>Add</button>
+      <button onClick={clearCompletedTodos}>Clear</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+  )
 }
 
-export default App;
+export default App
